8534d8e094feadcbbc80c3a072af3706308fe8e8
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-02-27 20:03:53 +0100

    style

--- client/src/components/signup.tsx
+++ client/src/components/signup.tsx
@@ -5,6 +5,7 @@ import { FaArrowRight } from 'react-icons/fa'
 const SignUp = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
 
   const navigate = useNavigate()
 
@@ -16,26 +17,37 @@ const SignUp = () => {
       }}
       className="loginForm"
     >
-      <label className="loginForm_label">
-       Veuillez saisir votre email
-      </label>
+      <label className="loginForm_label">Veuillez saisir votre email</label>
       <input
         value={email}
         type="email"
         placeholder="example@hotmail.fr"
+        id="email"
+        name="email"
         required
         onChange={(e) => setEmail(e.target.value)}
       />
 
-      <label className="loginForm_label">
-        Choisir votre mot de passe
-      </label>
+      <label className="loginForm_label">Choisir votre mot de passe</label>
       <input
         value={password}
         type="password"
         placeholder="Azerty123$"
+        id="password"
+        name="password"
         required
         onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <label className="loginForm_label">Confirmer le mot de passe :</label>
+      <input
+        value={confirmPassword}
+        type="password"
+        placeholder="Azerty123$"
+        id="confirmPassword"
+        name="confirmPassword"
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        required
       />
 
       <button type="submit">
--- client/src/pages/root.tsx
+++ client/src/pages/root.tsx
@@ -1,16 +1,46 @@
+import React, { useState } from 'react'
 import SignIn from '../components/signin'
 import SignUp from '../components/signup'
 import Logo from '../../src/assets/img/icon-left-font-monochrome-white.svg'
 
-
 export const test = 'test'
 
-const Root = () => {
+const Root = (props) => {
+  const [signUpModal, setSignUpModal] = useState(props.signup)
+  const [signInModal, setSignInModal] = useState(props.signin)
+
+  //fonction pour gérer les modals
+  const handleModals = (e) => {
+    if (e.target.id === 'signin') {
+      setSignUpModal(false)
+      setSignInModal(true)
+    } else if (e.target.id === 'signup') {
+      setSignUpModal(true)
+      setSignInModal(false)
+    }
+  }
   return (
     <main>
-      <img src={Logo} alt='Logo groupomania'/>
-      <SignUp />
-      <SignIn />
+      <img src={Logo} alt="Logo groupomania" />
+      <div className="connection_form_btn">
+        <button
+          id="signin"
+          onClick={handleModals}
+          className={signInModal ? 'connection_form_btn_signin' : null}
+        >
+          Connexion
+        </button>
+        <br />
+        <button
+          id="signup"
+          onClick={handleModals}
+          className={signUpModal ? 'connection_form_btn_signup' : null}
+        >
+          Inscription
+        </button>
+      </div>
+      {signUpModal && <SignUp />}
+      {signInModal && <SignIn />}
     </main>
   )
 }
